3797ed710baccc2fec45dc6303324dfb0340e5c7
crodaycat <lgiraldomu@gmail.com>
2022-02-27 12:46:40 -0500

    feat: integrate wifi, mqtt and curtains to main program

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,49 @@
-#include <ESP32Servo.h>
+// #include <ESP32Servo.h>
 
-Servo myservo; // create servo object to control a servo
-// 16 servo objects can be created on the ESP32
+#include <PIWiFI.h>
+#include <PIMQTT.h>
+#include <PICurtains.h>
 
-int pos = 0; // variable to store the servo position
-// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
-// Possible PWM GPIO pins on the ESP32-S2: 0(used by on-board button),1-17,18(used by on-board LED),19-21,26,33-42
+char *wifiSSID = "CLARO_WIFI1F5";
+char *wifiPassword = "CLAROI743";
+char *mqttServerIP = "192.168.0.112";
+int mqttPort = 1883;
+char *mqttClientID = "CurtainsClient";
 
-int servoPin = 17;
+PIWiFi *wiFiController;
+PIMQTT *mqttController;
+PICurtains *curtainsManager;
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]");
+  Serial.println();
+
+  String message;
+
+  for (int i = 0; i < length; i++)
+  {
+    message += (char)payload[i];
+  }
+
+  Serial.println(message);
+  curtainsManager->handle(message);
+}
 
 void setup()
 {
   Serial.begin(115200);
 
-  Serial.println(servoPin);
-
-  // Allow allocation of all timers
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
-
-  myservo.setPeriodHertz(50); // standard 50 hz servo
-
-  myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
-  // using default min/max of 1000us and 2000us
-  // different servos may require different min/max settings
-  // for an accurate 0 to 180 sweep
+  wiFiController = new PIWiFi(wifiSSID, wifiPassword, 500);
+  curtainsManager = new PICurtains();
+  mqttController = new PIMQTT(mqttServerIP, mqttClientID, mqttPort);
+  mqttController->_mqttClient->setCallback(callback);
 }
 
 void loop()
 {
-
-  for (pos = 0; pos <= 180; pos += 1)
-  { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    Serial.println(pos);
-    myservo.write(pos); // tell servo to go to position in variable 'pos'
-    delay(15);          // waits 15ms for the servo to reach the position
-  }
-
-  delay(1000);
-  for (pos = 180; pos >= 0; pos -= 1)
-  { // goes from 180 degrees to 0 degrees
-    Serial.println(pos);
-    myservo.write(pos); // tell servo to go to position in variable 'pos'
-    delay(15);          // waits 15ms for the servo to reach the position
-  }
-
-  Serial.println("Hola mundo");
-
-  delay(1000);
+  wiFiController->loop();
+  mqttController->loop();
 }
